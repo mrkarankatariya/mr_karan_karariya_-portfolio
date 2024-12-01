f572e32e7e8bee8eb8be2b90f29fe7ebe91e0824
mrkarankatariya <kk8729852@gmail.com>
2024-12-01 12:15:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let option ={
+    strings:["Full Stack Developer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+};
+let typed = new Typed(".text",option);
